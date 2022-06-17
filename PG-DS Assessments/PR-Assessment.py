57cbe92346283c600d2f47e8c1d9f7d511cfa0fd
Layth-ele <layth.ele@gmail.com>
2022-06-16 22:20:41 -0700

    Python assessment

--- PG-DS Assessments/PR-Assessment.py
+++ PG-DS Assessments/PR-Assessment.py
@@ -5,7 +5,6 @@ Created on 2 Jun 2022
 '''
 
 #1#  Create a bank class with the following attributes:(IFSC_Code ,bankname ,branchname ,loc)
-from xmlrpc.client import boolean
 
 class Bank:
     def __init__(self ,IFSC_Code = 0000 ,bankname = 'ABC' ,branchname = 'DEF' ,loc = 9999):
@@ -13,6 +12,8 @@ class Bank:
         self.bankname = bankname
         self.branchname = branchname
         self.loc = loc
+        
+#2# Create a customer class with the following attributes(CustomerID, custname, address, contactdetails)
 
 class Customer:
     def __init__(self, customerID = 00000, custname = "GHI", address= "JKL", contactdetails =987654321):
@@ -21,71 +22,86 @@ class Customer:
         self.address = address
         self.contactdetails = contactdetails
 
+#3# Create an account class that inherits from bank class with the following attributes (Use Super () to pass value to the base class):
+#(AccountID ,Cust Object of Customer ,balance
+
+#Add the following methods to get account information, withdraw, and deposit:
+
+# getAccountInfo()
+# deposit(2000,'true')
+# widthraw(500)
+# getBalance()
+
 
 class Account(Bank):
-    def __init__(self, accountID = 89839832389 ,custObjectOfCustomer = 'None' ,balance = 5000):
+    def __init__(self, accountID = 89839832389 ,custObjectOfCustomer = 'None' ,balance = 5000 ,chequing= True):
         super().__init__()
+        
         self.accountID = accountID
         self.custObjectOfCustomer = custObjectOfCustomer
         self.balance = balance
+        self.chequing = chequing
 
     def getAccountInfo(self):
         print("Bank Name " + str(self.bankname))
         print("Branch Name " + str(self.branchname))
         print("Account ID" + str(self.accountID))
         
+        
     def getBalance(self):
-        return self.balance
-
-    def deposit(self, dep = 2000, chequing= True):
-        self.dep = dep
-        self.chequing = True
         if self.chequing == True :
-                print('Account Type : Chequing Account')
+            print('Account Type : Chequing Account')
+        return self.balance
+        
+    
+
+    def deposit(self, dep = 2000):
+        self.dep = dep
+        
         print ('You deposit a ' + '{}'.format(dep) + ' to your chequing account . \n' + 'Your total balance is :', self.balance + self.dep)
 
     def withdraw(self, withd = 500):
         self.withd = withd
         print ('You withdraw a ' + '{}'.format(withd) + ' from your chequing account . \n' + 'Your total balance is :', self.balance - self.withd)
 
+
+#4# Create a SavigsAccount class that inherits from the account with the following attributes (Use Super () to
+#pass valued to the base class):( SMinBalance)
+
         
-class SavigsAccount (Bank):
-    def __init__(self,accountID = 89839832389 ,SMinBalance = 2003 ,balance = 2000 ):
+class SavigsAccount (Account):
+    def __init__(self ,SMinBalance = 5 ,saving = True ):
         super().__init__()
+        
         self.SMinBalance = SMinBalance
-        self.balance = balance
-        self.saving = True
-        if SMinBalance > balance: 
-            print('You have not enough money in your saving account')     
-            
+        self.saving = saving
+        if saving == True:
+           print('Account Type : Saving Account')
+        
     def getSavingBalance(self):
         return self.balance
-        
-    def savingDeposit(self, dep = 2000, saving= True):
-        if self.saving == True :
-            print('Account Type : Saving Account')
-        self.dep = dep
-        print ('You deposit a ' + '{}'.format(dep) + ' to your saving account . \n' + 'Your total balance is :', self.balance + self.dep)
+    
+    def savingDeposit(self):
+        self.dep = int(input("Enter the amount you want to deposit : USA "))
+        print ('You deposit a ' + '{}'.format(self.dep) + ' $'+ ' to your saving account . \n' + 'Your total balance is :', self.balance + self.dep)
 
-    def savingWithdraw(self, withd = 500):
-        self.withd = withd
-        print ('You withdraw a ' + '{}'.format(withd) + ' from your saving account . \n' + 'Your total balance is :', self.balance - self.withd)
-                    
-
-
-
+    def savingWithdraw(self):
+        self.withd = int(input("Enter the amount you want to withdraw: USA "))
+        if (self.balance - self.withd < 0): #Validate MinBalance before allowing withdrawals.
+            print("Error Amount not available in card.")
+        else :
+            print ('You withdraw a ' + '{}'.format(self.withd) + ' $' + ' from your saving account . \n' + 'Your total balance is :', self.balance - self.withd)
             
-
-
 account1 = Account()
+print('\n')         
 account1.getAccountInfo()
-print('\n')
+print('\n')        
 print("Total balance: " + str(account1.getBalance()) + ' $ ')
 print('\n')
 account1.deposit()
 print('\n')
 account1.withdraw()
-
+print('\n')
 account2 = SavigsAccount()
 account2.getSavingBalance()
 print('\n')
