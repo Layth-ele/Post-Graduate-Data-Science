e9fca1298c4a66276ae345aed11cf9e0f6cdbc43
Layth-ele <layth.ele@gmail.com>
2022-06-17 12:03:03 -0700

    finishing up bank account program assessment using python

--- PG-DS Assessments/PR-Assessment.py
+++ PG-DS Assessments/PR-Assessment.py
@@ -34,35 +34,39 @@ class Customer:
 
 
 class Account(Bank):
-    def __init__(self, accountID = 89839832389 ,custObjectOfCustomer = 'None' ,balance = 5000 ,chequing= True):
+    def __init__(self, accountID = 89839832389 ,custObjectOfCustomer = 'None' ,balance = 5000 ,type = True):
         super().__init__()
         
         self.accountID = accountID
         self.custObjectOfCustomer = custObjectOfCustomer
         self.balance = balance
-        self.chequing = chequing
+        self.type = type
 
     def getAccountInfo(self):
         print("Bank Name " + str(self.bankname))
         print("Branch Name " + str(self.branchname))
         print("Account ID" + str(self.accountID))
+        print('\n')
         
-        
-    def getBalance(self):
-        if self.chequing == True :
+        if self.type == True :
             print('Account Type : Chequing Account')
-        return self.balance
+            print('Account Balance :', self.balance)
+
         
-    
+        
+    def getBalance(self): # calculate the total balance 
+        balance = self.balance +self.dep - self.withd
+        print("Total balance: " + str(balance) + ' $ ')
+
 
     def deposit(self, dep = 2000):
         self.dep = dep
         
-        print ('You deposit a ' + '{}'.format(dep) + ' to your chequing account . \n' + 'Your total balance is :', self.balance + self.dep)
+        print ('You deposit a ' + '{}'.format(dep) + ' to your chequing account . \n' + 'Your balance is :', self.balance + self.dep)
 
     def withdraw(self, withd = 500):
         self.withd = withd
-        print ('You withdraw a ' + '{}'.format(withd) + ' from your chequing account . \n' + 'Your total balance is :', self.balance - self.withd)
+        print ('You withdraw a ' + '{}'.format(withd) + ' from your chequing account . \n' + 'Your New Balance is :', self.balance - self.withd)
 
 
 #4# Create a SavigsAccount class that inherits from the account with the following attributes (Use Super () to
@@ -70,43 +74,59 @@ class Account(Bank):
 
         
 class SavigsAccount (Account):
-    def __init__(self ,SMinBalance = 5 ,saving = True ):
+    def __init__(self ,SMinBalance = 5 ,type = False ):    
         super().__init__()
         
         self.SMinBalance = SMinBalance
-        self.saving = saving
-        if saving == True:
+        self.type = type
+        if type == False:
            print('Account Type : Saving Account')
-        
-    def getSavingBalance(self):
-        return self.balance
-    
+           print('Account Balance :', self.balance)
+
+
     def savingDeposit(self):
         self.dep = int(input("Enter the amount you want to deposit : USA "))
         print ('You deposit a ' + '{}'.format(self.dep) + ' $'+ ' to your saving account . \n' + 'Your total balance is :', self.balance + self.dep)
 
-    def savingWithdraw(self):
+    def savingWithdraw(self):        
         self.withd = int(input("Enter the amount you want to withdraw: USA "))
-        if (self.balance - self.withd < 0): #Validate MinBalance before allowing withdrawals.
+        
+        SMinBalance = self.balance - self.withd #Validate MinBalance before allowing withdrawals.
+
+        if (SMinBalance < 0): 
             print("Error Amount not available in card.")
         else :
-            print ('You withdraw a ' + '{}'.format(self.withd) + ' $' + ' from your saving account . \n' + 'Your total balance is :', self.balance - self.withd)
+            print ('You withdraw a ' + '{}'.format(self.withd) + ' $' + ' from your saving account . \n' + 'Your New Balance Is :', SMinBalance )
+            
+    def getSavingBalance(self):# calculate the total balance
+        balance = self.balance +self.dep - self.withd
+        if(balance > 0):    
+            print(" balance: " + str(balance) + ' $ ')
+        else:
+            print("Transaction can\'t be completed!")
+            
+            
+            
+            ####################################################################################################
+            
+                                                      #Test the Code#
+            ####################################################################################################
+                                                                                  
             
 account1 = Account()
 print('\n')         
 account1.getAccountInfo()
-print('\n')        
-print("Total balance: " + str(account1.getBalance()) + ' $ ')
 print('\n')
 account1.deposit()
 print('\n')
 account1.withdraw()
 print('\n')
+account1.getBalance()
+print('\n')        
 account2 = SavigsAccount()
-account2.getSavingBalance()
-print('\n')
-print("Total balance: " + str(account2.getSavingBalance()) + ' $ ')
 print('\n')
 account2.savingDeposit()
 print('\n')
 account2.savingWithdraw()
+print('\n')
+account2.getSavingBalance()
